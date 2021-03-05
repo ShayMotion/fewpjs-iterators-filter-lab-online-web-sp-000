1970240afb5baa2508e4127d310a251a913b080c
Shayan Mohsin <shaymotionfilms@gmail.com>
2021-03-05 14:44:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+const findMatching = (drivers, name) => {
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
 }
 
-const superbowlWin = array => {
-  let result = array.find(element => element.result === "W");
-  return !!result ? result.year : undefined;
+const fuzzyMatch = (drivers, name) => {
+  return drivers.filter(driver => driver.toLowerCase().slice(0, name.length) === name.toLowerCase());
+}
+
+const matchName = (drivers, name) => {
+  return drivers.filter(driver => driver.name === name);
 }
